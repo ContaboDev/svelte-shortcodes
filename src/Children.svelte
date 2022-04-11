0b7c2dea5580dd84f5faf018841b4a8f1ec63c19
Bogdan Vladutu <bogdanvladutu@gmail.com>
2022-04-11 13:01:26 +0300

    Wrap Text Node in span parent - Google Translate Errors - Ref Ecommerce #889

--- src/Children.svelte
+++ src/Children.svelte
@@ -8,7 +8,9 @@
 </script>
 
 {#each nodes as node (node)}
-  {#if node.nodeType == TEXT_NODE}{node.textContent}{:else if node.nodeType == ELEMENT_NODE}
+  {#if node.nodeType == TEXT_NODE}
+    <span>{node.textContent}</span>
+  {:else if node.nodeType == ELEMENT_NODE}
     {#if components[node.tagName]}
       <svelte:component this={components[node.tagName]} {...node.attrs}
         ><svelte:self nodes={node.childNodes} {components} /></svelte:component
